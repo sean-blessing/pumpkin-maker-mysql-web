3aa777210e437ae7c522fa2bc95616fec4714385
Sean Blessing <snblessing@gmail.com>
2020-10-16 13:30:34 -0400

    init init

--- src/main/java/com/pumpkin/db/PumpkinRepo.java
+++ src/main/java/com/pumpkin/db/PumpkinRepo.java
@@ -1,9 +1,10 @@
 package com.pumpkin.db;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.pumpkin.business.Pumpkin;
 
-public interface PumpkinRepo extends CrudRepository<Pumpkin, Integer> {
+public interface PumpkinRepo extends JpaRepository<Pumpkin, Integer> {
 
 }
--- src/main/java/com/pumpkin/web/PumpkinController.java
+++ src/main/java/com/pumpkin/web/PumpkinController.java
@@ -1,17 +1,52 @@
 package com.pumpkin.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.pumpkin.business.Pumpkin;
+import com.pumpkin.db.PumpkinRepo;
 
+@CrossOrigin
 @RestController
-@RequestMapping("/tests")
+@RequestMapping("/pumpkins")
 public class PumpkinController {
 	
-
-	@GetMapping("/pumpkin")
-	public Pumpkin makePumpkin() {
-		Pumpkin p = new Pumpkin(1,"orange",11.5, 12, 15, true);
-		return p;
+	@Autowired
+	private PumpkinRepo pumpkinRepo;
+	
+	// list all pumpkins
+	@GetMapping("/")
+	public List<Pumpkin> getAllPumpkins() {
+		return pumpkinRepo.findAll();
 	}
+	
+	// add a pumpkin
+	@PostMapping("/")
+	public Pumpkin addPumpkin(Pumpkin p) {
+		if (pumpkinRepo.save(p)!=null) {
+			System.out.println("Pumpkin save was successful");
+		}
+		else {
+			System.out.println("Pumpkin save ISSUE.");
+		}
+		return p;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
